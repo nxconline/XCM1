9d566857d1fd51733d32682bcf215e6e57bd6cd9
nanxiance <nxconline@126.com>
2021-12-04 17:23:52 +0800

    添加项目文件。

---
+++ 电子式断路器/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace 电子式断路器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.knob1 = new 电子式断路器.Knob();
+            this.SuspendLayout();
+            // 
+            // knob1
+            // 
+            this.knob1.Diamter = 100;
+            this.knob1.Location = new System.Drawing.Point(318, 218);
+            this.knob1.Name = "knob1";
+            this.knob1.ScaleNum = 10;
+            this.knob1.Scales = new string[] {
+        "250",
+        "250",
+        "250",
+        "250",
+        "250",
+        "250",
+        "250",
+        "250",
+        "250",
+        "250"};
+            this.knob1.Size = new System.Drawing.Size(130, 130);
+            this.knob1.TabIndex = 0;
+            this.knob1.Text = "knob1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.knob1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Knob knob1;
+    }
+}
+
---
+++ 电子式断路器/Knob.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 电子式断路器
+{
+    public partial class Knob : Control
+    {
+        public Knob()
+        {
+            InitializeComponent();
+            this.Diamter = 100;
+            this.Size = new Size(Diamter+30, Diamter+30);
+            this.ScaleNum = 10;
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+        public int Diamter { get; set; }
+        public int ScaleNum { get; set; }
+        public string[] Scales { get; set; }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            Graphics g = pe.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            using(Pen pen = new Pen(Color.Black,2))
+            {
+                //绘制外圆               
+                Rectangle rect = new Rectangle(15, 15, this.Diamter, this.Diamter);
+                rect.Inflate(-(int)pen.Width, -(int)pen.Width);
+                g.DrawEllipse(pen, rect);
+                //绘制刻度
+                float radius = this.Diamter / 2;
+                Matrix matrix = new Matrix();
+                matrix.Translate(this.Width / 2, this.Width / 2);
+                g.Transform = matrix;
+                for (int i = 0; i < ScaleNum; i++)
+                {
+                    g.DrawLine(pen, new PointF(0, radius-pen.Width), new PointF(0, radius- pen.Width-10));
+                    matrix.RotateAt(360/ScaleNum, new PointF(0, 0));
+                    g.Transform = matrix;
+                }
+                matrix.Reset();
+                //绘制数字
+                for (int i = 0; i < ScaleNum; i++)
+                {
+                    Scales = new string[] {"250", "250", "250", "250", "250", "250", "250", "250", "250", "250" };
+                    float x = (float)((this.Width/2) * Math.Cos(-90 + i * 360 / ScaleNum));
+                    float y = (float)((this.Width / 2) * Math.Sin(-90 + i * 360 / ScaleNum));
+                    g.DrawString(this.Scales[i], this.Font, Brushes.Black, new PointF(x,y));
+                }
+            }
+
+            base.OnPaint(pe);
+        }
+    }
+}
